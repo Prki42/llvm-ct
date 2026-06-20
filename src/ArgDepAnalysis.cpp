2975e4ec8dcf120f9035f30f01b77d386cf8423c
Aleksa Prtenjača <aleksa.prtenjaca03@gmail.com>
2026-06-21 01:50:41 +0200

    removed redundant llvm:: qualifiers in cpp files

--- src/ArgDepAnalysis.cpp
+++ src/ArgDepAnalysis.cpp
@@ -13,7 +13,7 @@ AnalysisKey ArgDepAnalysis::Key;
 
 ArgDepResult ArgDepAnalysis::run(Function &F, FunctionAnalysisManager &) {
   ArgDepResult res;
-  std::queue<llvm::Value *> valueWorklist;
+  std::queue<Value *> valueWorklist;
 
   auto mark = [&res, &valueWorklist](Value *to_be_marked) {
     // avoid adding already marked instructions to worklist
@@ -33,10 +33,10 @@ ArgDepResult ArgDepAnalysis::run(Function &F, FunctionAnalysisManager &) {
     valueWorklist.pop();
 
     for (auto *User : currValue->users()) {
-      if (auto *I = llvm::dyn_cast<llvm::Instruction>(User)) {
+      if (auto *I = dyn_cast<Instruction>(User)) {
 
         // handle store separately
-        if (auto *store_inst = llvm::dyn_cast<llvm::StoreInst>(I)) {
+        if (auto *store_inst = dyn_cast<StoreInst>(I)) {
           auto *val_op = store_inst->getValueOperand();
           // if value operand is marked, mark the ptr operand
           if (res.MarkedValues.count(val_op)) {
@@ -54,7 +54,7 @@ ArgDepResult ArgDepAnalysis::run(Function &F, FunctionAnalysisManager &) {
 
   // gathering all vulnerable branches
   for (auto &BB : F) {
-    auto *BI = llvm::dyn_cast<llvm::BranchInst>(BB.getTerminator());
+    auto *BI = dyn_cast<BranchInst>(BB.getTerminator());
     if (BI && BI->isConditional()) {
       if (res.MarkedValues.count(BI->getCondition())) {
         res.MarkedBranches.insert(BI);
@@ -65,32 +65,32 @@ ArgDepResult ArgDepAnalysis::run(Function &F, FunctionAnalysisManager &) {
   return res;
 }
 
-PreservedAnalyses ArgDepPrinterPass::run(llvm::Function &F,
-                                         llvm::FunctionAnalysisManager &FAM) {
+PreservedAnalyses ArgDepPrinterPass::run(Function &F,
+                                         FunctionAnalysisManager &FAM) {
   // We get the analysis results
   auto &Res = FAM.getResult<ArgDepAnalysis>(F);
 
-  llvm::errs() << "\n--- ArgDepAnalysis Debug: " << F.getName() << " ---\n";
+  errs() << "\n--- ArgDepAnalysis Debug: " << F.getName() << " ---\n";
 
   // We write out all the branches
-  llvm::errs() << "Found " << Res.MarkedBranches.size()
-               << " conditionals which need CFL:\n";
+  errs() << "Found " << Res.MarkedBranches.size()
+         << " conditionals which need CFL:\n";
 
   for (auto *BI : Res.MarkedBranches) {
-    llvm::errs() << "  [!] Conditional: " << *BI << "\n";
+    errs() << "  [!] Conditional: " << *BI << "\n";
   }
 
   // We write out all the values which are marked
-  llvm::errs() << "Number of values: " << Res.MarkedValues.size() << "\n";
+  errs() << "Number of values: " << Res.MarkedValues.size() << "\n";
   // As well as all the instructions that contain the marked values
   for (auto *V : Res.MarkedValues) {
-    if (auto *I = llvm::dyn_cast<llvm::Instruction>(V)) {
-      llvm::errs() << "    Marked Value: " << *I << "\n";
+    if (auto *I = dyn_cast<Instruction>(V)) {
+      errs() << "    Marked Value: " << *I << "\n";
     }
   }
-  llvm::errs() << "------------------------------------------\n";
+  errs() << "------------------------------------------\n";
 
-  return llvm::PreservedAnalyses::all();
+  return PreservedAnalyses::all();
 }
 
 } // namespace ct
